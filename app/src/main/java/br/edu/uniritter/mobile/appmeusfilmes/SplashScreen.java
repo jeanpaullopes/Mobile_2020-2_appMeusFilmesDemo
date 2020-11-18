f6567d17693ee513e3d31a5dc23c081d449f55aa
Prof Jean Paul <jean.paul@uniritter.edu.br>
2020-11-18 20:29:53 -0300

    uso de camera, intents para whatsapp

--- app/src/main/java/br/edu/uniritter/mobile/appmeusfilmes/SplashScreen.java
+++ app/src/main/java/br/edu/uniritter/mobile/appmeusfilmes/SplashScreen.java
@@ -4,12 +4,24 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.biometric.BiometricPrompt;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -20,6 +32,11 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.Executor;
 
 import br.edu.uniritter.mobile.appmeusfilmes.model.Categoria;
@@ -130,8 +147,8 @@ public class SplashScreen extends AppCompatActivity implements Response.Listener
         // Create the text message with a string
         Intent sendIntent = new Intent();
         sendIntent.setAction(Intent.ACTION_SEND);
-        sendIntent.putExtra("json", "{\"id\":40096}");
-        sendIntent.setType("application/json");
+        sendIntent.putExtra("json","{\"id\":40096}");
+       sendIntent.setType("application/json");
 
         // Verify that the intent will resolve to an activity
         if (sendIntent.resolveActivity(getPackageManager()) != null) {
@@ -141,6 +158,131 @@ public class SplashScreen extends AppCompatActivity implements Response.Listener
                     "Sem Activity para abrir application/json", Toast.LENGTH_SHORT).show();
         }
     }
+
+    public void enviarWhatsApp(View v) {
+        PackageManager pm=getPackageManager();
+        try {
+
+            Intent waIntent = new Intent(Intent.ACTION_SEND);
+            waIntent.setType("text/plain");
+            String text = "uniritterfilmes.edu.br/filme?id=40097";
+
+            PackageInfo info=pm.getPackageInfo("com.whatsapp", PackageManager.GET_META_DATA);
+            waIntent.setPackage("com.whatsapp");
+
+            waIntent.putExtra(Intent.EXTRA_TEXT, text);
+            startActivity(waIntent);
+
+        } catch (PackageManager.NameNotFoundException e) {
+            Toast.makeText(this, "WhatsApp não instalado", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private static final int SOLICITAR_PERMISSAO = 1;
+    public void checarPermissao(View v){
+
+        // Verifica  o estado da permissão de WRITE_EXTERNAL_STORAGE
+        int permissionCheck = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            // Se for diferente de PERMISSION_GRANTED, então vamos exibir a tela padrão
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, SOLICITAR_PERMISSAO);
+        } else {
+            // Senão vamos compartilhar a imagem
+            sharedImage();
+        }
+    }
+
+    private void sharedImage(){
+        // Vamos carregar a imagem em um bitmap
+        ImageView im = findViewById(R.id.imageViewLogo);
+        Drawable drawable = im.getDrawable();
+        Bitmap b = ((BitmapDrawable)drawable).getBitmap();
+        Intent share = new Intent(Intent.ACTION_SEND);
+        //setamos o tipo da imagem
+        share.setType("image/jpeg");
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        // comprimimos a imagem
+        b.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        // Gravamos a imagem
+        String path = MediaStore.Images.Media.insertImage(getContentResolver(), b, "Titulo da Imagem", null);
+        // criamos uam Uri com o endereço que a imagem foi salva
+        Uri imageUri =  Uri.parse(path);
+        // Setamos a Uri da imagem
+        share.putExtra(Intent.EXTRA_STREAM, imageUri);
+        // chama o compartilhamento
+        startActivity(Intent.createChooser(share, "Selecione"));
+    }
+
+
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    public void tirarFoto(View v) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+        @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+            super.onActivityResult(requestCode, resultCode, intent);
+            if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+                try {
+                    Bundle extras = intent.getExtras();
+                    Bitmap imageBitmap = (Bitmap) extras.get("data");
+                    ImageView im = findViewById(R.id.imageViewLogo);
+                    im.setImageBitmap(imageBitmap);
+                } catch (NullPointerException ex) {}
+            }
+        }
+
+    String currentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    public void dispatchTakePictureIntent(View v) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                //aqui vai o mesmo endereço do Manifest
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "br.edu.uniritter.mobile.appmeusfilmes.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
     public void onClickFinger(View v) {
         executor = ContextCompat.getMainExecutor(this);
         biometricPrompt = new BiometricPrompt(this,
@@ -152,6 +294,9 @@ public class SplashScreen extends AppCompatActivity implements Response.Listener
                 Toast.makeText(getApplicationContext(),
                         "Authentication error: " + errString, Toast.LENGTH_SHORT)
                         .show();
+                //Intent i = new Intent(SplashScreen.this,
+                //       MainActivity.class);
+                //startActivity(i);
             }
 
             @Override
@@ -175,9 +320,9 @@ public class SplashScreen extends AppCompatActivity implements Response.Listener
         });
 
         promptInfo = new BiometricPrompt.PromptInfo.Builder()
-                .setTitle("Biometric login for my app")
-                .setSubtitle("Log in using your biometric credential")
-                .setNegativeButtonText("Use account password")
+                .setTitle("Login por Biometria")
+                .setSubtitle("Use sua credencial biométrica do Dispositivo para logar")
+                .setNegativeButtonText("Usar usuario e senha")
                 .build();
 
         // Prompt appears when user clicks "Log in".
